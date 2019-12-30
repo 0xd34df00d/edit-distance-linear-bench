f3f95fd7d6d4ecec5b850c8c6a9d549844e00cae
0xd34df00d <0xd34df00d@gmail.com>
2019-12-30 18:48:55 -0500

    Better names, less typing

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,23 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
-import qualified Text.EditDistance.Linear01Pure as L01P
-import qualified Text.EditDistance.Linear01PureStrict as L01PS
-import qualified Text.EditDistance.Linear01PureStrictLLVM as L01PSL
-import qualified Text.EditDistance.Linear02PureUnsafe as L02P
-import qualified Text.EditDistance.Linear02PureUnsafeStrict as L02PS
-import qualified Text.EditDistance.Linear02PureUnsafeStrictLLVM as L02PSL
+import qualified Text.EditDistance.Linear01Pure as L01
+import qualified Text.EditDistance.Linear01PureStrict as L01S
+import qualified Text.EditDistance.Linear01PureStrictLLVM as L01SL
+import qualified Text.EditDistance.Linear02PureUnsafe as L02
+import qualified Text.EditDistance.Linear02PureUnsafeStrict as L02S
+import qualified Text.EditDistance.Linear02PureUnsafeStrictLLVM as L02SL
 import System.Environment
 
 main :: IO ()
 main = do
   [impl] <- getArgs
-  let func | impl == "L01P" = L01P.levenshteinDistance
-           | impl == "L01PS" = L01PS.levenshteinDistance
-           | impl == "L01PSL" = L01PSL.levenshteinDistance
-           | impl == "L02P" = L02P.levenshteinDistance
-           | impl == "L02PS" = L02PS.levenshteinDistance
-           | impl == "L02PSL" = L02PSL.levenshteinDistance
+  let func | impl == "1" = L01.levenshteinDistance
+           | impl == "1S" = L01S.levenshteinDistance
+           | impl == "1SL" = L01SL.levenshteinDistance
+           | impl == "2" = L02.levenshteinDistance
+           | impl == "2S" = L02S.levenshteinDistance
+           | impl == "2SL" = L02SL.levenshteinDistance
            | otherwise = error "Unknown implementation"
   let s1 = BS.replicate len 'a'
   let s2 = s1
