49e741523e4167da25017d39ac1ed55bfaa78a21
0xd34df00d <0xd34df00d@gmail.com>
2019-12-30 18:35:45 -0500

    Added pure/unsafe implementations

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,9 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Text.EditDistance.Linear01Pure as L01P
 import qualified Text.EditDistance.Linear01PureStrict as L01PS
 import qualified Text.EditDistance.Linear01PureStrictLLVM as L01PSL
+import qualified Text.EditDistance.Linear02PureUnsafe as L02P
+import qualified Text.EditDistance.Linear02PureUnsafeStrict as L02PS
+import qualified Text.EditDistance.Linear02PureUnsafeStrictLLVM as L02PSL
 import System.Environment
 
 main :: IO ()
@@ -12,6 +15,9 @@ main = do
   let func | impl == "L01P" = L01P.levenshteinDistance
            | impl == "L01PS" = L01PS.levenshteinDistance
            | impl == "L01PSL" = L01PSL.levenshteinDistance
+           | impl == "L02P" = L02P.levenshteinDistance
+           | impl == "L02PS" = L02PS.levenshteinDistance
+           | impl == "L02PSL" = L02PSL.levenshteinDistance
            | otherwise = error "Unknown implementation"
   let s1 = BS.replicate len 'a'
   let s2 = s1
---
+++ src/Text/EditDistance/Linear02PureUnsafeStrictLLVM.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE Strict #-}
+{-# OPTIONS_GHC -fllvm #-}
+
+module Text.EditDistance.Linear02PureUnsafeStrictLLVM where
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BS
+import qualified Data.Vector.Unboxed as V
+import Data.List
+
+levenshteinDistance :: BS.ByteString -> BS.ByteString -> Int
+levenshteinDistance s1 s2 = foldl' outer (V.generate (n + 1) id) [0 .. m - 1] V.! n
+  where
+    m = BS.length s1
+    n = BS.length s2
+
+    outer v0 i = V.constructN (n + 1) ctr
+      where
+        s1char = s1 `BS.unsafeIndex` i
+        ctr v1 | V.length v1 == 0 = i + 1
+        ctr v1 = min (substCost + substCostBase) $ 1 + min delCost insCost
+          where
+            j = V.length v1
+            delCost = v0 `V.unsafeIndex` j
+            insCost = v1 `V.unsafeIndex` (j - 1)
+            substCostBase = v0 `V.unsafeIndex` (j - 1)
+            substCost = if s1char == s2 `BS.unsafeIndex` (j - 1) then 0 else 1
