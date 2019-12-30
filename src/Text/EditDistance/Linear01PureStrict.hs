e9e9d7dbd88195cde62d6211d5d3608afb289bc3
0xd34df00d <0xd34df00d@gmail.com>
2019-12-30 18:24:49 -0500

    Added more variants of the initial pure version

---
+++ src/Text/EditDistance/Linear01PureStrict.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE Strict #-}
+
+module Text.EditDistance.Linear01PureStrict where
+
+import qualified Data.ByteString as BS
+import qualified Data.Vector.Unboxed as V
+import Data.List
+
+levenshteinDistance :: BS.ByteString -> BS.ByteString -> Int
+levenshteinDistance s1 s2 = foldl' outer (V.generate (n + 1) id) [0 .. m - 1] V.! n
+  where
+    m = BS.length s1
+    n = BS.length s2
+
+    outer v0 i = V.constructN (n + 1) ctr
+      where
+        s1char = s1 `BS.index` i
+        ctr v1 | V.length v1 == 0 = i + 1
+        ctr v1 = min (substCost + substCostBase) $ 1 + min delCost insCost
+          where
+            j = V.length v1
+            delCost = v0 V.! j
+            insCost = v1 V.! (j - 1)
+            substCostBase = v0 V.! (j - 1)
+            substCost = if s1char == s2 `BS.index` (j - 1) then 0 else 1
